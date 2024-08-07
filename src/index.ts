8a2f2ec1dba6e6cf3b943909286ca08acf718e07
Eduardo Pedrosa <eduardobaptista66@gmail.com>
2024-08-07 15:51:12 -0300

    cors enabledd all requests

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import helmet from 'helmet';
 import session from 'express-session';
 import 'reflect-metadata';
 import { setupSwagger } from './swagger';
-import cors, { CorsOptions, CorsOptionsDelegate } from 'cors';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -15,25 +15,6 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 const mongooseConnection = process.env.PRIVATE_MONGOOSE_CONNECTION;
 const expiryDate = new Date(Date.now() + 60 * 60 * 1000); // 1 hour
-
-// Lê as URLs permitidas do arquivo .env
-// const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
-
-// Função para verificar se a origem está na lista de permitidas
-// const corsOptions: CorsOptions | CorsOptionsDelegate<Request> = {
-//   origin: (origin, callback) => {
-//     if (origin && allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else if (!origin) {
-//       // Permite solicitações sem origem (como cURL ou Postman)
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-// };
 
 async function main() {
   if (!mongooseConnection) return;
@@ -53,14 +34,8 @@ app.use(
     cookie: { secure: true, httpOnly: true, expires: expiryDate },
   })
 );
+app.use(cors());
 app.use(router);
-app.use(
-  cors({
-    origin: 'http://localhost:5173', // Substitua pelo endereço do seu frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-);
 setupSwagger(app);
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
